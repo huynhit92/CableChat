c28aa69c8d907fc2dc545231e6aa9a00848a95f7
Pham Tran Huynh <phamtranhuynh92@gmail.com>
2017-02-18 12:45:24 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://your_app.herokuapp.com',
-                                               'http://your_app.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://kunaguero.herokuapp.com',
+                                               'http://kunaguero.herokuapp.com']
   
-  config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
+  config.action_cable.url = "wss://kunaguero-actioncable.herokuapp.com/cable"
 end
